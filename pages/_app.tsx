b51648d46ba47c1eb0d724a856b953bd1f0066a7
Elias Garcia <eliasgarcia2206@gmail.com>
2021-01-24 19:59:12 -0500

    setup ts and styled-components

---
+++ pages/_app.tsx
@@ -0,0 +1,44 @@
+// style imports
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+// type imports
+import type { AppProps } from 'next/app'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+    Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    line-height: 1.6;
+    font-size: 18px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+// Override App provide for nextjs
+// App is used to initialize pages
+// Custom app: control the page initialization
+
+// Global style is provided
+
+export default function App({ Component, pageProps } : AppProps) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
